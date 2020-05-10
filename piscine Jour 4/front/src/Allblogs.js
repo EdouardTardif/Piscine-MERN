c7b92faadf37023ad6f016916aa36784cb2f1293
unknown <edouardnoel.tardif@gmail.com>
2020-05-10 23:32:40 +0200

    profile + billet

--- piscine Jour 4/front/src/Allblogs.js
+++ piscine Jour 4/front/src/Allblogs.js
@@ -17,6 +17,7 @@ class Allblogs extends React.Component {
             error : [],
             isloggedin : false,
             billets : null,
+            resultat : [],
         }
         
     }
@@ -43,6 +44,7 @@ class Allblogs extends React.Component {
                 //     let cur2 = this.state.billets.allblogs.resul
                 //     console.log(cur[x], cur2[x][0]);
                 // }
+                this.test();
             } else {
                 const error = new Error(response.error);
                 throw error;
@@ -50,12 +52,14 @@ class Allblogs extends React.Component {
     }
 
     test = () => {
+        let array = [];
         for(let x in this.state.billets.allblogs.resultat){
             let cur = this.state.billets.allblogs.resultat;
             let cur2 = this.state.billets.allblogs.resul
-            console.log(cur[x], cur2[x][0]);
+            array.push({ billet : cur[x] , creator : cur2[x][0]});
         }
-        return <li>test</li>
+        this.setState({resultat : array});
+        console.log(this.state.resultat);
     }
 
     render(){
@@ -66,8 +70,11 @@ class Allblogs extends React.Component {
         <div>
             <h1>Myblog CONTROLLER</h1>
                 <ul>
-                        { this.state.billets != null && this.state.billets.allblogs.resultat.length > 0 ? this.test
-                        : <li key='test'> Vous n'avez aucun billet</li>}
+                        { this.state.billets != null && this.state.resultat.length > 0 ? this.state.resultat.map( e => 
+                            <Link to={"/"+ e.creator.login +"/" + e.billet._id} >
+                                <li> { e.billet.titre }</li>
+                            </Link>
+                        ) : <li key='test'> Vous n'avez aucun billet</li>}
                 </ul>
         </div>
         )
---
+++ piscine Jour 4/front/src/Billetinformation.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route, Redirect,} from 'react-router-dom';
+
+
+
+class Billetinformation extends React.Component {
+
+    constructor(props){
+        super();
+    
+        this.state = {
+            titre: null,
+            content : null,
+            error : [],
+            isloggedin : false,
+            billets : null,
+        }
+        
+    }
+    
+    componentDidMount(){
+        this.fetchblog();
+        // console.log(this.props.match.params.id)
+    }
+
+    fetchblog = async () => {
+            const form = {
+                id : this.props.match.params.id,
+            }
+            const response = await axios.post( 'http://localhost:4242/billet/info', form, { headers: { 'Content-Type': 'application/json' } } )
+            // console.log(response);
+            if(response.status == 200){
+                const data = response.data;
+                this.setState({billets : data.billetinfo});
+                this.setState({titre : this.state.billets.titre, content : this.state.billets.titre});
+
+            } else {
+                const error = new Error(response.error);
+                throw error;
+            }
+    }
+
+
+    deletebillet = async () => {
+        const form = {
+            id : this.props.match.params.id,
+        }
+        const response = await axios.post( 'http://localhost:4242/billet/delete', form, { headers: { 'Content-Type': 'application/json' } } )
+        console.log(response);
+        if(response.status == 200){
+            this.props.history.push('/myblog');
+        } else {
+            const error = new Error(response.error);
+            throw error;
+        }
+    }
+
+    handleInputChange = e => {
+        this.setState({
+          [e.target.name]: e.target.value,
+        });
+     //    console.log(this.state);
+    };
+
+    changeinfo = async () => {
+            const form = {
+                id : this.state.billets._id,
+                titre : this.state.titre,
+                content : this.state.content,
+            }
+            const response = await axios.post( 'http://localhost:4242/billet/update', form, { headers: { 'Content-Type': 'application/json' } } )
+            if(response.status == 200){
+                const data = response.data;
+                window.location.reload(false);
+
+
+            } else {
+                const error = new Error(response.error);
+                throw error;
+            }
+           
+            console.log(response.data);
+    }
+
+    render(){
+        if(this.state.isloggedin){
+            this.props.history.push('/profile');
+        }
+        return (
+        <div>
+            <h1>Billetinfo CONTROLLER</h1>
+
+            { this.state.billets != null ?  <input onChange={this.handleInputChange} name="titre" defaultValue={this.state.billets.titre}></input> : <h1> Vous n'avez aucun billet</h1>}<br></br>
+            { this.state.billets != null ?  <textarea onChange={this.handleInputChange} name="content" defaultValue={this.state.billets.content}></textarea> : null}<br></br>
+            { this.state.billets != null ? <button onClick={this.changeinfo}>CHANGER LE BILLET</button> : null} 
+            { this.state.billets != null ? <button onClick={this.deletebillet}>SUPPRIMER LE BILLET</button> : null }
+        </div>
+        )
+    }
+}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+export default Billetinformation;
